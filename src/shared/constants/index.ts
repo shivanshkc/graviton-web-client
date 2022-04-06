a266261a24edbe6c07a7107f6013158366fb1f48
Luke Green <lukeg496@gmail.com>
2022-04-05 19:29:55 -0500

    Support click and hold to increase size of dots

--- src/shared/constants/index.ts
+++ src/shared/constants/index.ts
@@ -5,5 +5,7 @@ export const gravitationalConstant = 100;
 export const fps = 10000;
 export const deltaTime = 1000 / fps;
 
-export const defaultDotDiameter = 15;
+export const defaultDotDiameter = 8;
 export const defaultDotMass = 1;
+
+export const defaultIncrementRatio = 0.03;
